461e6d0a16d45877a6a2af3afe39426a0b3f4bcb
Vitaliy Kairachka <difo89nur@gmail.com>
2024-04-09 01:07:57 +0300

    Initial commit

---
+++ src/main/java/com/kairachka/bugreportservice/BugReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kairachka.bugreportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugReportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugReportServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kairachka/bugreportservice/BugReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairachka.bugreportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugReportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
